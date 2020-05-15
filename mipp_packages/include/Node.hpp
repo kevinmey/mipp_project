37540940623f3a54e433f0520a14e96c630e654f
kevinmey <kevinme@stud.ntnu.no>
2020-05-15 09:34:09 +0200

    Move Node definition to header

--- mipp_packages/include/Node.hpp
+++ mipp_packages/include/Node.hpp
@@ -18,13 +18,33 @@ public:
   int rank_;
   bool is_goal_;
 
-  Node(double x = 0, double y = 0, double z = 0, double yaw = 0, double cost = 0, double gain = 0, int id = 0, Node* parent = NULL, int rank = 0, bool is_goal = false);
+  Node(double x = 0, double y = 0, double z = 0, double yaw = 0, double cost = 0, double gain = 0, int id = 0, Node* parent = NULL, int rank = 0, bool is_goal = false) {
+    this->position_.x = x;
+    this->position_.y = y;
+    this->position_.z = z;
+    this->yaw_ = yaw;
+    this->cost_ = cost;
+    this->gain_ = gain;
+    this->id_ = id;
+    this->parent_ = parent;
+    this->rank_ = rank;
+    this->is_goal_ = is_goal;
+  }
 
-  Node* getParent();
-  void setParent(Node* node);
+  Node* getParent() { return this->parent_; }
+  void setParent(Node* node) { this->parent_ = node; }
 
-  void operator=(Node p);
-  bool operator==(Node p);
-  bool operator!=(Node p);
-  bool operator<(Node p);
+  void operator=(Node p) {
+    this->position_ = p.position_;
+    this->yaw_ = p.yaw_;
+    this->cost_ = p.cost_;
+    this->gain_ = p.gain_;
+    this->id_ = p.id_;
+    this->parent_ = p.parent_;
+    this->rank_ = p.rank_;
+    this->is_goal_ = p.is_goal_;
+  }
+  bool operator==(Node p) { return (this->id_ == p.id_); }
+  bool operator!=(Node p) { return (this->id_ != p.id_); }
+  bool operator<(Node p) { return (this->cost_ < p.cost_); }
 };
