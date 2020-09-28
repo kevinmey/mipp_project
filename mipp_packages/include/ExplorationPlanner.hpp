81cc5b1050aa4138525df75ba30096e924df8c35
kevinmey <kevinme@stud.ntnu.no>
2020-09-28 16:14:48 +0200

    Rename to exploration planner

--- mipp_packages/include/ExplorationPlanner.hpp
+++ mipp_packages/include/ExplorationPlanner.hpp
@@ -10,51 +10,49 @@
 #include "mipp_msgs/ExplorationPath.h"
 #include "mipp_msgs/ExplorationPose.h"
 #include <mipp_msgs/StartExplorationAction.h>
+#include <mipp_msgs/MoveVehicleAction.h>
 #include <actionlib/client/simple_action_client.h>
 #include <actionlib/client/terminal_state.h>
 
 #include <geometry_msgs/PointStamped.h>
+#include <nav_msgs/Path.h>
 
 #include <string>
 #include <cmath> /* sqrt, pow */
  
-class ExplorationServer
+class ExplorationPlanner
 {
 public:
   // Constructor
-  ExplorationServer(ros::NodeHandle n, ros::NodeHandle np);
+  ExplorationPlanner(ros::NodeHandle n, ros::NodeHandle np);
   // Destructor
-  ~ExplorationServer();
-  
-  /* 
-  *  Publish functions for publishers
-  */
-  
-  /* 
-  *  Callback functions for subscriptions
-  */
-  void subClickedPoint(const geometry_msgs::PointStampedConstPtr& clicked_point_msg);
-
-  /* 
-  *  Planner functions
-  */
-  
-  /* 
-  *  Utility functions
-  */
-  void getParams(ros::NodeHandle np);
+  ~ExplorationPlanner();
   
 private:
+  // Functions
+  // Publish functions for publishers
+  // Callback functions for subscriptions
+  void subClickedPoint(const geometry_msgs::PointStampedConstPtr& clicked_point_msg);
+  // Planner functions
+  // Utility functions
+  void getParams(ros::NodeHandle np);
+  nav_msgs::Path makePathFromExpPath(mipp_msgs::ExplorationPath);
+
+  // Variables
   // Publishers
+  ros::Publisher pub_ugv_goal_;
+  ros::Publisher pub_ugv_goal_path_;
   //// UGV
   //// UAVs
   // Subscribers
   ros::Subscriber sub_clicked_point_;
   // Actionlib
   actionlib::SimpleActionClient<mipp_msgs::StartExplorationAction>* act_ugv_exploration_client_;
-  //std::vector<actionlib::SimpleActionClient<mipp_msgs::StartExplorationAction>*> act_uav_exploration_clients_;
+  std::vector<actionlib::SimpleActionClient<mipp_msgs::StartExplorationAction>*> act_uav_exploration_clients_;
+  std::vector<actionlib::SimpleActionClient<mipp_msgs::MoveVehicleAction>*> act_uav_move_vehicle_clients_;
   // Parameters
   int nr_of_uavs_;
+  std::string uav_world_frame_;
   std::string ugv_ns_;
   // Variable
   bool running_exploration_;
